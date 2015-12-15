c7b1600f56187a68cdf72a415eed1ec4cb667fc3
sam <satiricallaughter@gmail.com>
2015-12-15 14:46:55 +0000

    removed old files

--- utils/get_jpgs_in_dir.m
+++
@@ -1,8 +0,0 @@
-function img_names = get_jpgs_in_dir(path)
-    %List all files in the given directory with .jpg extension
-    img_files = dir(fullfile(path, '*.jpg')); 
-    
-    % create a cell array of the full paths to each image
-    img_names = {img_files.name}';
-    paths = repmat({path}, size(img_names));
-    img_names = strcat(paths, filesep, img_names);
