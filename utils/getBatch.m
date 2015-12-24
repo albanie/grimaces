6ed6f51cafc03255aeb5f11bacd7a1cae5b6de74
sam <satiricallaughter@gmail.com>
2015-12-24 07:39:15 +0000

    Ensured that data is moved to the gpu during batching.

--- utils/getBatch.m
+++ utils/getBatch.m
@@ -1,9 +1,15 @@
-function inputs = getBatch(imdb, batch)
+function inputs = getBatch(imdb, batch, opts)
 % returns the inputs and associated labels in the 
 % batch specified by 'batch', contained in 'imdb'.
 
 input = imdb.images.data(:,:,:,batch);
 label = imdb.images.labels(1,batch);
+
+% Move input to the GPU if needed.
+if opts.numGpus > 0
+  input = gpuArray(input) ;
+end
+
 inputs = { 'input', input, ...
            'label', label};
 end
