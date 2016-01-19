1be42667a9b01ee9c9e720e42b6c8190ec6034d0
sam <satiricallaughter@gmail.com>
2016-01-19 12:16:48 +0000

    updated pretraining choices

--- dag/initPretrainedNet.m
+++ dag/initPretrainedNet.m
@@ -5,6 +5,10 @@ if strcmp(opts.pretrainedNet, 'Alexnet')
     dagnet = initAlexnet(opts);
 end
 
+if strcmp(opts.pretrainedNet, 'ScaledAlexnet')
+    dagnet = initScaledAlexnet(opts);
+end
+
 if strcmp(opts.pretrainedNet, 'Randomnet')
     dagnet = initRandomnet(opts);
 end
